33d3b01321eb3c19f945786b4bc5c8301573a366
Christian Koopmann <christian@MTMAC0057.local>
2022-08-12 13:40:29 +0800

    Simple example test

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Contract {
+    uint public count;
+
+    function add(uint a) public {
+        count += a;
+    }
+    
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract instance;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        instance = new Contract();
+    }
+
+    function testAdd() public {
+        uint a = 1;
+        uint countBefore = instance.count();
+        instance.add(a);
+        uint countAfter = instance.count();
+        assertEq(countAfter, countBefore + a);
     }
 }
